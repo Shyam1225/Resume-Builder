ce2dfef7902239d00f51bc3b81593641d49626cc
Shyam1225 <damanihit@gmail.com>
2025-06-28 19:06:28 +0530

    Server Started

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+//connect DB
+
+//middleware
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Started on http://localhost:${PORT}`);
+})
